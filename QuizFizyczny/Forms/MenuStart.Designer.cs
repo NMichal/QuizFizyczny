4d466a8bfc95c28596fde9bb67125d033f21ea98
Michal N <michal.niewolik@gmail.com>
2018-01-08 20:14:56 +0100

    Menu

---
+++ QuizFizyczny/Forms/MenuStart.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace QuizFizyczny.Forms
+{
+    partial class MenuStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bttnSingle = new System.Windows.Forms.Button();
+            this.bttnMulti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nazwa gracza";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(142, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // bttnSingle
+            // 
+            this.bttnSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bttnSingle.Location = new System.Drawing.Point(62, 110);
+            this.bttnSingle.Name = "bttnSingle";
+            this.bttnSingle.Size = new System.Drawing.Size(162, 46);
+            this.bttnSingle.TabIndex = 2;
+            this.bttnSingle.Text = "Jeden gracz";
+            this.bttnSingle.UseVisualStyleBackColor = true;
+            this.bttnSingle.Click += new System.EventHandler(this.bttnSingle_Click);
+            // 
+            // bttnMulti
+            // 
+            this.bttnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bttnMulti.Location = new System.Drawing.Point(62, 188);
+            this.bttnMulti.Name = "bttnMulti";
+            this.bttnMulti.Size = new System.Drawing.Size(162, 46);
+            this.bttnMulti.TabIndex = 3;
+            this.bttnMulti.Text = "Wielu graczy";
+            this.bttnMulti.UseVisualStyleBackColor = true;
+            this.bttnMulti.Click += new System.EventHandler(this.bttnMulti_Click);
+            // 
+            // MenuStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bttnMulti);
+            this.Controls.Add(this.bttnSingle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "MenuStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bttnSingle;
+        private System.Windows.Forms.Button bttnMulti;
+    }
+}
