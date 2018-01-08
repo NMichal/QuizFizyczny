cbb62a4b9ea7e6080a465244361325055c59e29e
Mateusz Lindel <mateusz.lindel@smcebi.edu.pl>
2018-01-08 20:44:32 +0100

    dodanie klasy z TODO xD

---
+++ QuizFizyczny/TODO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizFizyczny
+{
+    class TODO
+    {
+        ///logowanie i rejestracja - MATEK
+        ///menu Start z wyborem gry - single czy multi
+        ///baza - postawimy na VPS i postaram się to połączyć z ENTITY
+    }
+}
