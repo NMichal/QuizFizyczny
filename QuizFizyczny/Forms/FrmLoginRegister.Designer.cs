cce8d63511b91436effc7f8d6549fe0f6b508c66
Mateusz Lindel <mateusz.lindel@smcebi.edu.pl>
2018-01-12 09:08:44 +0100

    - poprawka widoku formatli logującej/rejestującej - usuniecie pola z nazwa gracza z menu start

--- QuizFizyczny/Forms/FrmLoginRegister.Designer.cs
+++ QuizFizyczny/Forms/FrmLoginRegister.Designer.cs
@@ -31,24 +31,24 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLoginRegister));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.TPLogin = new System.Windows.Forms.TabPage();
-            this.TPRegister = new System.Windows.Forms.TabPage();
-            this.BtnLogin = new System.Windows.Forms.Button();
-            this.TbLogin = new System.Windows.Forms.TextBox();
-            this.TbHaslo = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TbHaslo = new System.Windows.Forms.TextBox();
+            this.TbLogin = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.TPRegister = new System.Windows.Forms.TabPage();
+            this.BtnRegister = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TbPowtozHaslo = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.TbHasloRegister = new System.Windows.Forms.TextBox();
             this.TbLoginRegister = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.TbPowtozHaslo = new System.Windows.Forms.TextBox();
-            this.BtnRegister = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.TPLogin.SuspendLayout();
-            this.TPRegister.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.TPRegister.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -56,6 +56,7 @@
             this.tabControl1.Controls.Add(this.TPLogin);
             this.tabControl1.Controls.Add(this.TPRegister);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -70,75 +71,23 @@
             this.TPLogin.Controls.Add(this.TbHaslo);
             this.TPLogin.Controls.Add(this.TbLogin);
             this.TPLogin.Controls.Add(this.BtnLogin);
-            this.TPLogin.Location = new System.Drawing.Point(4, 22);
+            this.TPLogin.Location = new System.Drawing.Point(4, 27);
             this.TPLogin.Name = "TPLogin";
             this.TPLogin.Padding = new System.Windows.Forms.Padding(3);
-            this.TPLogin.Size = new System.Drawing.Size(589, 289);
+            this.TPLogin.Size = new System.Drawing.Size(589, 284);
             this.TPLogin.TabIndex = 0;
             this.TPLogin.Text = "Logowanie";
             this.TPLogin.UseVisualStyleBackColor = true;
             // 
-            // TPRegister
+            // pictureBox1
             // 
-            this.TPRegister.Controls.Add(this.BtnRegister);
-            this.TPRegister.Controls.Add(this.label5);
-            this.TPRegister.Controls.Add(this.TbPowtozHaslo);
-            this.TPRegister.Controls.Add(this.label3);
-            this.TPRegister.Controls.Add(this.label4);
-            this.TPRegister.Controls.Add(this.TbHasloRegister);
-            this.TPRegister.Controls.Add(this.TbLoginRegister);
-            this.TPRegister.Location = new System.Drawing.Point(4, 22);
-            this.TPRegister.Name = "TPRegister";
-            this.TPRegister.Padding = new System.Windows.Forms.Padding(3);
-            this.TPRegister.Size = new System.Drawing.Size(589, 289);
-            this.TPRegister.TabIndex = 1;
-            this.TPRegister.Text = "Rejestracja";
-            this.TPRegister.UseVisualStyleBackColor = true;
-            // 
-            // BtnLogin
-            // 
-            this.BtnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnLogin.Location = new System.Drawing.Point(491, 250);
-            this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(90, 31);
-            this.BtnLogin.TabIndex = 0;
-            this.BtnLogin.Text = "Zaloguj i graj!";
-            this.BtnLogin.UseVisualStyleBackColor = true;
-            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
-            // 
-            // TbLogin
-            // 
-            this.TbLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TbLogin.Location = new System.Drawing.Point(169, 123);
-            this.TbLogin.Name = "TbLogin";
-            this.TbLogin.Size = new System.Drawing.Size(226, 20);
-            this.TbLogin.TabIndex = 1;
-            // 
-            // TbHaslo
-            // 
-            this.TbHaslo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TbHaslo.Location = new System.Drawing.Point(169, 168);
-            this.TbHaslo.Name = "TbHaslo";
-            this.TbHaslo.PasswordChar = '*';
-            this.TbHaslo.Size = new System.Drawing.Size(226, 20);
-            this.TbHaslo.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(120, 123);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Login";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(228, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
@@ -153,15 +102,104 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Hasło";
             // 
-            // pictureBox1
+            // label1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(228, 17);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(104, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(120, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login";
+            // 
+            // TbHaslo
+            // 
+            this.TbHaslo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbHaslo.Location = new System.Drawing.Point(169, 168);
+            this.TbHaslo.Name = "TbHaslo";
+            this.TbHaslo.PasswordChar = '*';
+            this.TbHaslo.Size = new System.Drawing.Size(226, 24);
+            this.TbHaslo.TabIndex = 2;
+            // 
+            // TbLogin
+            // 
+            this.TbLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbLogin.Location = new System.Drawing.Point(169, 123);
+            this.TbLogin.Name = "TbLogin";
+            this.TbLogin.Size = new System.Drawing.Size(226, 24);
+            this.TbLogin.TabIndex = 1;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BtnLogin.Location = new System.Drawing.Point(491, 245);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(90, 31);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "Zaloguj i graj!";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // TPRegister
+            // 
+            this.TPRegister.Controls.Add(this.BtnRegister);
+            this.TPRegister.Controls.Add(this.label5);
+            this.TPRegister.Controls.Add(this.TbPowtozHaslo);
+            this.TPRegister.Controls.Add(this.label3);
+            this.TPRegister.Controls.Add(this.label4);
+            this.TPRegister.Controls.Add(this.TbHasloRegister);
+            this.TPRegister.Controls.Add(this.TbLoginRegister);
+            this.TPRegister.Location = new System.Drawing.Point(4, 27);
+            this.TPRegister.Name = "TPRegister";
+            this.TPRegister.Padding = new System.Windows.Forms.Padding(3);
+            this.TPRegister.Size = new System.Drawing.Size(589, 284);
+            this.TPRegister.TabIndex = 1;
+            this.TPRegister.Text = "Rejestracja";
+            this.TPRegister.UseVisualStyleBackColor = true;
+            // 
+            // BtnRegister
+            // 
+            this.BtnRegister.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BtnRegister.Location = new System.Drawing.Point(491, 245);
+            this.BtnRegister.Name = "BtnRegister";
+            this.BtnRegister.Size = new System.Drawing.Size(90, 31);
+            this.BtnRegister.TabIndex = 11;
+            this.BtnRegister.Text = "Zarejestruj";
+            this.BtnRegister.UseVisualStyleBackColor = true;
+            this.BtnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(14, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Powtórz hasło";
+            // 
+            // TbPowtozHaslo
+            // 
+            this.TbPowtozHaslo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbPowtozHaslo.Location = new System.Drawing.Point(116, 89);
+            this.TbPowtozHaslo.Name = "TbPowtozHaslo";
+            this.TbPowtozHaslo.PasswordChar = '*';
+            this.TbPowtozHaslo.Size = new System.Drawing.Size(226, 24);
+            this.TbPowtozHaslo.TabIndex = 9;
             // 
             // label3
             // 
@@ -197,7 +235,7 @@
             this.TbHasloRegister.Location = new System.Drawing.Point(116, 51);
             this.TbHasloRegister.Name = "TbHasloRegister";
             this.TbHasloRegister.PasswordChar = '*';
-            this.TbHasloRegister.Size = new System.Drawing.Size(226, 20);
+            this.TbHasloRegister.Size = new System.Drawing.Size(226, 24);
             this.TbHasloRegister.TabIndex = 6;
             // 
             // TbLoginRegister
@@ -207,43 +245,8 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TbLoginRegister.Location = new System.Drawing.Point(116, 6);
             this.TbLoginRegister.Name = "TbLoginRegister";
-            this.TbLoginRegister.Size = new System.Drawing.Size(226, 20);
+            this.TbLoginRegister.Size = new System.Drawing.Size(226, 24);
             this.TbLoginRegister.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(14, 92);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 17);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Powtórz hasło";
-            // 
-            // TbPowtozHaslo
-            // 
-            this.TbPowtozHaslo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.TbPowtozHaslo.Location = new System.Drawing.Point(116, 89);
-            this.TbPowtozHaslo.Name = "TbPowtozHaslo";
-            this.TbPowtozHaslo.PasswordChar = '*';
-            this.TbPowtozHaslo.Size = new System.Drawing.Size(226, 20);
-            this.TbPowtozHaslo.TabIndex = 9;
-            // 
-            // BtnRegister
-            // 
-            this.BtnRegister.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.BtnRegister.Location = new System.Drawing.Point(491, 250);
-            this.BtnRegister.Name = "BtnRegister";
-            this.BtnRegister.Size = new System.Drawing.Size(90, 31);
-            this.BtnRegister.TabIndex = 11;
-            this.BtnRegister.Text = "Zarejestuj się";
-            this.BtnRegister.UseVisualStyleBackColor = true;
-            this.BtnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
             // 
             // FrmLoginRegister
             // 
@@ -257,9 +260,9 @@
             this.tabControl1.ResumeLayout(false);
             this.TPLogin.ResumeLayout(false);
             this.TPLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.TPRegister.ResumeLayout(false);
             this.TPRegister.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- QuizFizyczny/Forms/MenuStart.Designer.cs
+++ QuizFizyczny/Forms/MenuStart.Designer.cs
@@ -28,29 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.bttnSingle = new System.Windows.Forms.Button();
             this.bttnMulti = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(27, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nazwa gracza";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(142, 46);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
             // 
             // bttnSingle
             // 
@@ -81,22 +61,16 @@
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.bttnMulti);
             this.Controls.Add(this.bttnSingle);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
             this.MaximumSize = new System.Drawing.Size(300, 300);
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "MenuStart";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button bttnSingle;
         private System.Windows.Forms.Button bttnMulti;
     }
