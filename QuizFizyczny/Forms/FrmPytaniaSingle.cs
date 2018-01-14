a835ef8f75a5e9994c64de37aa3b78acd2cc936d
Michal N <michal.niewolik@gmail.com>
2018-01-14 14:51:49 +0100

    Dodanie singleplayer

---
+++ QuizFizyczny/Forms/FrmPytaniaSingle.cs
@@ -0,0 +1,81 @@
+﻿using QuizFizyczny.DataBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizFizyczny.Forms
+{
+    public partial class FrmPytaniaSingle : Form
+    {
+        List<int> listOidPytan = null;
+        int liczbaPytan = 0;
+        int aktualnyNumerPytania = 0;
+        List<RadioButton> listRB;
+        public FrmPytaniaSingle(List<int> oidPytania)
+        {
+            InitializeComponent();
+            listRB = new List<RadioButton>(new RadioButton[] { radioButton1, radioButton2, radioButton3, radioButton4 });
+            listOidPytan = oidPytania;
+            liczbaPytan = oidPytania.Count;
+            WyswietlPytanie(aktualnyNumerPytania);
+        }
+
+        public void WyswietlPytanie(int numerPytania)
+        {
+            int oidPytania = listOidPytan[numerPytania];
+            Pytania pytanieObj = ContextDb.contextDB.Pytania.Where(a => a.id == oidPytania).FirstOrDefault();
+            textBoxPytanie.Text = pytanieObj.trescPytania;
+            int rbcount = 0;
+            foreach (var item in pytanieObj.Odpowiedzi)
+            {
+                listRB[rbcount].Text = item.odpowiedz;
+                listRB[rbcount].Checked = false; //Odznaczenie poprzedniej odpowiedzi
+                rbcount++;
+            }
+        }
+
+        private void bttnOdpowiedz_Click(object sender, EventArgs e)
+        {
+            //Sprawdz czy zaznaczono odp
+            if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+            {
+                //Policz punkty
+
+                if (aktualnyNumerPytania + 1 < liczbaPytan)
+                {
+                    aktualnyNumerPytania++;
+                    WyswietlPytanie(aktualnyNumerPytania);
+                }
+                else
+                {                    
+                    MessageBox.Show("Quiz zakończony", "Koniec");
+                    //Liczba punktów: .... Zająłeś ... miejsce.
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Proszę zaznaczyć odpowiedź", "Nie wybrano odpowiedzi");
+            }
+        }
+
+        private void bttnZakoncz_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Czy na pewno chcesz zakończyć?", "Zakończ Quiz", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }
+        }
+    }
+}
---
+++ QuizFizyczny/UserControls/UcQuizSingle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuizFizyczny.DataBase;
+
+namespace QuizFizyczny.UserControls
+{
+    public partial class UcQuizSingle : UserControl
+    {
+        List<int> listOidPytan = null;
+        int liczbaPytan = 0;
+        int aktualnyNumerPytania = 0;
+        List<RadioButton> listRB;
+        public UcQuizSingle(List<int> oidPytania)
+        {
+            InitializeComponent();
+            listRB = new List<RadioButton>(new RadioButton[] { radioButton1, radioButton2, radioButton3, radioButton4 });
+            listOidPytan = oidPytania;
+            liczbaPytan = oidPytania.Count;
+            WyswietlPytanie(aktualnyNumerPytania);
+        }
+
+        public void WyswietlPytanie(int numerPytania)
+        {
+            int oidPytania = listOidPytan[numerPytania];
+            Pytania pytanieObj = ContextDb.contextDB.Pytania.Where(a => a.id == oidPytania).FirstOrDefault();
+            textBoxPytanie.Text = pytanieObj.trescPytania;
+            int rbcount = 0;
+            foreach (var item in pytanieObj.Odpowiedzi)
+            {
+                listRB[rbcount].Text = item.odpowiedz;
+                listRB[rbcount].Checked = false; //Odznaczenie poprzedniej odpowiedzi
+                rbcount++;
+            }
+        }
+
+        private void bttnOdpowiedz_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+            {
+                //Policz punkty
+
+                if (aktualnyNumerPytania + 1 < liczbaPytan)
+                {
+                    aktualnyNumerPytania++;
+                    WyswietlPytanie(aktualnyNumerPytania);
+                }
+                else
+                {
+                    MessageBox.Show("Quiz zakończony", "Koniec");
+                    //Liczba punktów: .... Zająłeś ... miejsce.
+                    //this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Proszę zaznaczyć odpowiedź", "Nie wybrano odpowiedzi");
+            }
+        }
+
+        private void bttnZakoncz_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
